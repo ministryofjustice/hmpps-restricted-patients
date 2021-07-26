683f38cd95da20e8539f3b8f4f497da06b3db2ae
John Robertson <jsrobertson@gmail.com>
2021-07-26 08:38:26 +0100

    NN-3517 Add prisonApi test

--- server/data/prisonApiClient.ts
+++ server/data/prisonApiClient.ts
@@ -1,6 +1,7 @@
 import type { Readable } from 'stream'
 
 import config from '../config'
+import logger from '../../logger'
 import RestClient from './restClient'
 
 export interface Prison {
@@ -20,6 +21,10 @@ export default class PrisonApiClient {
   async getPrisonerImage(prisonerNumber: string): Promise<Readable> {
     return this.restClient.stream({
       path: `/api/bookings/offenderNo/${prisonerNumber}/image/data`,
+      errorLogger: error =>
+        error.status === 404
+          ? logger.info(`No prisoner image available for prisonerNumber: ${prisonerNumber}`)
+          : this.restClient.defaultErrorLogger(error),
     }) as Promise<Readable>
   }
 }
