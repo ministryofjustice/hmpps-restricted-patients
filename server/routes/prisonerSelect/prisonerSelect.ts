7c6a96acd985336476bc513e9cf272cc0c88096a
John Robertson <jsrobertson@gmail.com>
2021-07-27 08:52:12 +0100

    NN-3517 Add search again box to top of select a prisoner

--- server/routes/prisonerSelect/prisonerSelect.ts
+++ server/routes/prisonerSelect/prisonerSelect.ts
@@ -1,27 +1,31 @@
+import url from 'url'
 import { Request, Response } from 'express'
 import { FormError } from '../../@types/template'
 import PrisonerSearchService, { PrisonerSearchSummary } from '../../services/prisonerSearchService'
+import validateForm from '../prisonerSearch/prisonerSearchValidation'
 
 type PageData = {
   error?: FormError
   searchResults?: PrisonerSearchSummary[]
+  searchTerm: string
 }
 
 export default class PrisonerSelectRoutes {
   constructor(private readonly prisonerSearchService: PrisonerSearchService) {}
 
   private renderView = async (req: Request, res: Response, pageData: PageData): Promise<void> => {
-    const { error, searchResults } = pageData
+    const { error, searchResults, searchTerm } = pageData
 
     return res.render('pages/prisonerSelect', {
       errors: error ? [error] : [],
       searchResults,
+      searchTerm,
     })
   }
 
   view = async (req: Request, res: Response): Promise<void> => {
     const { user } = res.locals
-    const searchTerm = JSON.stringify(req.query.searchTerm)
+    const searchTerm = JSON.stringify(req.query.searchTerm)?.replace(/"/g, '')
 
     if (!searchTerm) return res.redirect('/search-for-prisoner')
 
@@ -30,6 +34,21 @@ export default class PrisonerSelectRoutes {
       user
     )
 
-    return this.renderView(req, res, { searchResults })
+    return this.renderView(req, res, { searchResults, searchTerm })
+  }
+
+  submit = async (req: Request, res: Response): Promise<void> => {
+    const { searchTerm } = req.body
+
+    const error = validateForm({ searchTerm })
+
+    if (error) return this.renderView(req, res, { error, searchTerm })
+
+    return res.redirect(
+      url.format({
+        pathname: '/select-prisoner',
+        query: { searchTerm },
+      })
+    )
   }
 }
