ebd0c9e7f6cdbda477cd046a68e87c070b19d533
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-03-03 11:05:20 +0000

    NN-3782: Add gtag event function

---
+++ assets/js/googleAnalyticsEvents.js
@@ -0,0 +1,18 @@
+$(function () {
+  const $pathname = window.location.pathname
+
+  function sendEvent(action, category, label) {
+    gtag('event', action, {
+      event_category: category,
+      event_label: label,
+    })
+  }
+
+  if ($pathname.includes('prisoner-moved-to-hospital')) {
+    const hospital = window.location.pathname.split('/')[3]
+    sendEvent('Prisoner moved to hospital', 'Restricted patients movement', 'Prisoner moved to ' + hospital)
+  }
+  if ($pathname.includes('person-removed')) {
+    sendEvent('Prisoner removed from hospital', 'Restricted patients removal', 'Prisoner removed from hospital')
+  }
+})
