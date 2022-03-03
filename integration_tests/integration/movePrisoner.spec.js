794b67292a2e9a0a8f5d4a0b80a9b8951fc7e8f6
Natalie Clamp <natalie.clamp@digital.justice.gov.uk>
2022-03-03 12:15:07 +0000

    NN-3782: stub as empty function

--- integration_tests/integration/movePrisoner.spec.js
+++ integration_tests/integration/movePrisoner.spec.js
@@ -99,7 +99,7 @@ context('Move prisoner', () => {
 
   it('Completes a move prisoner journey', () => {
     // eslint-disable-next-line no-unused-vars
-    cy.stub().as('window.gtag')
+    cy.stub(() => {}).as('window.gtag')
     cy.visit('/search-for-prisoner')
     const prisonerSearchPage = PrisonerSearchPage.verifyOnPage()
     const prisonerSearchPageForm = prisonerSearchPage.form()
