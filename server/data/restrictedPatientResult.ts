c4ef490c3cdb43d9a59a97cf500e997607891a90
Nat Clamp <84783598+natclamp-moj@users.noreply.github.com>
2022-09-13 08:56:13 +0100

    NN-4498: Update deps (#142)
    * NN-4498: Update deps
    
    * NN-4498: Add eslint ignores for typescript oddities

--- server/data/restrictedPatientResult.ts
+++ server/data/restrictedPatientResult.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Expose } from 'class-transformer'
 
 export default class RestrictedPatientResult {
--- server/data/restrictedPatientSearchResult.ts
+++ server/data/restrictedPatientSearchResult.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Expose } from 'class-transformer'
 import PrisonerSearchResult from './prisonerSearchResult'
 
