5294c8c6b2a67f6dadf5264afd5cc547f57a9a18
petergphillips <peter@greenthistle.co.uk>
2022-09-16 10:14:42 +0100

    SDI-367: 🔒️ Fix path security as wasn't adding prefix (#151)

--- server/middleware/authorisationMiddleware.ts
+++ server/middleware/authorisationMiddleware.ts
@@ -8,11 +8,14 @@ export default function authorisationMiddleware(
   renderNotFound: boolean,
   authorisedRoles: string[] = []
 ): RequestHandler {
+  // authorities in the user token will always be prefixed by ROLE_.
+  // Convert roles that are passed into this function without the prefix so that we match correctly.
+  const authorisedAuthorities = authorisedRoles.map(role => (role.startsWith('ROLE_') ? role : `ROLE_${role}`))
   return asyncMiddleware((req, res, next) => {
     if (res.locals && res.locals.user && res.locals.user.token) {
-      const { authorities: roles = [] } = jwtDecode(res.locals.user.token) as { authorities?: string[] }
+      const { authorities = [] } = jwtDecode(res.locals.user.token) as { authorities?: string[] }
 
-      if (authorisedRoles.length && !roles.some(role => authorisedRoles.includes(role))) {
+      if (authorisedAuthorities.length && !authorities.some(authority => authorisedAuthorities.includes(authority))) {
         logger.error('User is not authorised to access this')
         return renderNotFound ? res.render('pages/notFound.njk') : res.redirect('/authError')
       }
