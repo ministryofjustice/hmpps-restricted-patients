ae8da33c247226100b6c4ef8bc2ee3d4a3aaf088
Peter Phillips <peter@greenthistle.co.uk>
2022-09-21 08:14:05 +0100

    SDI-345: ✨ Correct route name

--- server/routes/addPatient/hospitalSelect.ts
+++ server/routes/addPatient/hospitalSelect.ts
@@ -2,7 +2,7 @@ import HospitalSelectRoutes from '../selectHospital/hospitalSelect'
 import MovePrisonerService from '../../services/movePrisonerService'
 import PrisonerSearchService from '../../services/prisonerSearchService'
 
-export default class MovePrisonerHospitalSelectRoutes extends HospitalSelectRoutes {
+export default class AddPatientHospitalSelectRoutes extends HospitalSelectRoutes {
   constructor(movePrisonerService: MovePrisonerService, prisonerSearchService: PrisonerSearchService) {
     super(
       movePrisonerService,
