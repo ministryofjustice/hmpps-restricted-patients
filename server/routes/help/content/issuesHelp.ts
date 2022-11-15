c3f7c4dd9810678626892f07320db34ec6100ea7
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-11-15 15:11:28 +0000

    SDI-364 Help content tweak (#178)

--- server/routes/help/content/issuesHelp.ts
+++ server/routes/help/content/issuesHelp.ts
@@ -28,10 +28,9 @@ export default class IssuesHelp implements HelpContent {
     {
       id: 'not-released-to-hospital',
       category: this.category,
-      heading: `I can't add a released prisoner into the Restricted Patients service as they were not released to hospital.`,
+      heading: `How can I add a prisoner to Restricted Patients if they were not released to a hospital via classic NOMIS?`,
       content: `
-<p>The page "Add a released prisoner into restricted patients" can be used to add prisoners who were released to a hospital in Classic NOMIS.</p>
-<p>However if the prisoner was not released to hospital then they cannot be added to Restricted Patients. In this situation contact the Support team for advice.</p>
+<p>If the prisoner wasn't released to hospital then they cannot be added to Restricted Patients. In this situation contact the Support team for advice.</p>
         `.trim(),
     },
     {
@@ -40,8 +39,8 @@ export default class IssuesHelp implements HelpContent {
       heading: 'I am a POM and cannot find my offender who was booked out via the Restricted Patient service.',
       content: `
 <p>POMs can access their restricted patient offenders' records via their caseload in the MPC (POM) service.</p>
-<p>Firstly the POM needs to check that the offender has actually been allocated to them in the MPC service, as it seems many offenders have been booked into secure hospitals and a lot of key data has been missed from their records, meaning they have not been allocated in MPC, since they have not appeared, presumed ineligible by the service due to missing data.</p>
-<p>Also ensure that the prison caseload the offender was discharged from matches that of the POM, as since some splits (e.g. Parc) offenders are now assigned to the wrong caseloads.</p>
+<p>The POM needs to check that the offender has actually been allocated to them in the MPC service.  Many offenders have been booked into secure hospitals but key data was missing from their records.  This meant that they were not allocated in MPC as they were presumed ineligible due to missing data.</p>
+<p>Also ensure that the prison caseload the offender was discharged from matches that of the POM.  Following some splits (e.g. Parc), offenders were assigned to the wrong caseloads.</p>
 <p>If there is missing data on the offender's record which means that they are not showing in MPC, the POM should correct this. See the help on <a href="/help?section=updating-restricted-patient-in-nomis">updating restricted patients in NOMIS</a>.</p> 
 <p>For an offender to be visible for allocation in MPC they must be:</p>
 <ul>
@@ -49,7 +48,7 @@ export default class IssuesHelp implements HelpContent {
   <li>Over 18 years old (prisoners under 18 do not need a POM)</li>  
   <li>Not a civil case</li>  
   <li>Have an active sentence with a start date - sentenced prisoners must have the earliest release date or parole review date (CRD, ARD, TED, PED) in NOMIS</li>  
-  <li>If a lifer case sometimes the case record needs to be 'reset' by un-ticking and re-ticking the lifer box</li>  
+  <li>If a lifer case, sometimes the case record needs to be 'reset' by un-ticking and re-ticking the lifer box</li>  
   <li>Have their sentence calculation completed</li>  
 </ul>
         `.trim(),
