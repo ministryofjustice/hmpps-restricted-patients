2d0aa5a3367ccae2fd67f837bd1425d51b2f3a2b
Peter Phillips <peter@greenthistle.co.uk>
2022-12-05 15:24:57 +0000

    SDI-476: 🐛 Second attempt at bumping timeouts

--- server/config.ts
+++ server/config.ts
@@ -15,19 +15,12 @@ function get<T>(name: string, fallback: T, options = { requireInProduction: fals
 const requiredInProduction = { requireInProduction: true }
 
 export class AgentConfig {
-  // Maximum number of sockets to allow per host
-  maxSockets: 100
-
-  // Maximum number of sockets (per host) to leave open in a free state
-  maxFreeSockets: 10
-
-  // Sets the free socket to timeout after freeSocketTimeout milliseconds of inactivity on the free socket.
-  freeSocketTimeout: 30000
-
   // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket.
-  // This is deliberately set to a high value to allow superagent to time out the socket instead.
-  // The default is 8 seconds so if Prison API took > 8s then we were getting timeouts here when we didn't want them.
-  timeout: 60000
+  timeout: number
+
+  constructor(timeout = 8000) {
+    this.timeout = timeout
+  }
 }
 
 export interface ApiConfig {
@@ -64,7 +57,7 @@ export default {
         response: Number(get('HMPPS_AUTH_TIMEOUT_RESPONSE', 10000)),
         deadline: Number(get('HMPPS_AUTH_TIMEOUT_DEADLINE', 10000)),
       },
-      agent: new AgentConfig(),
+      agent: new AgentConfig(Number(get('HMPPS_AUTH_TIMEOUT_DEADLINE', 10000))),
       apiClientId: get('API_CLIENT_ID', 'clientid', requiredInProduction),
       apiClientSecret: get('API_CLIENT_SECRET', 'clientsecret', requiredInProduction),
       systemClientId: get('SYSTEM_CLIENT_ID', 'clientid', requiredInProduction),
@@ -76,15 +69,15 @@ export default {
         response: Number(get('PRISON_API_TIMEOUT_RESPONSE', 10000)),
         deadline: Number(get('PRISON_API_TIMEOUT_DEADLINE', 10000)),
       },
-      agent: new AgentConfig(),
+      agent: new AgentConfig(Number(get('PRISON_API_TIMEOUT_DEADLINE', 10000))),
     },
     prisonerSearch: {
       url: get('PRISONER_SEARCH_API_URL', 'http://localhost:8083', requiredInProduction),
       timeout: {
         response: Number(get('PRISONER_SEARCH_TIMEOUT_RESPONSE', 10000)),
-        deadline: Number(get('PRISONER_SEARCH_TIMEOUT_DEADLINE', 10000)),
+        deadline: Number(get('PRISONER_SEARCH_TIMEOUT_DEADLINE', 15000)),
       },
-      agent: new AgentConfig(),
+      agent: new AgentConfig(Number(get('PRISONER_SEARCH_TIMEOUT_DEADLINE', 15000))),
     },
     restrictedPatientApi: {
       url: get('RESTRICTED_PATIENT_API_URL', 'http://localhost:8084', requiredInProduction),
@@ -93,7 +86,7 @@ export default {
         // bumped to 30 seconds as prisoner re-index was taking > 15 seconds
         deadline: Number(get('RESTRICTED_PATIENT_API_TIMEOUT_DEADLINE', 30000)),
       },
-      agent: new AgentConfig(),
+      agent: new AgentConfig(Number(get('RESTRICTED_PATIENT_API_TIMEOUT_DEADLINE', 30000))),
     },
     tokenVerification: {
       url: get('TOKEN_VERIFICATION_API_URL', 'http://localhost:8100', requiredInProduction),
@@ -101,7 +94,7 @@ export default {
         response: Number(get('TOKEN_VERIFICATION_API_TIMEOUT_RESPONSE', 5000)),
         deadline: Number(get('TOKEN_VERIFICATION_API_TIMEOUT_DEADLINE', 5000)),
       },
-      agent: new AgentConfig(),
+      agent: new AgentConfig(Number(get('TOKEN_VERIFICATION_API_TIMEOUT_DEADLINE', 5000))),
       enabled: get('TOKEN_VERIFICATION_ENABLED', 'false') === 'true',
     },
   },
