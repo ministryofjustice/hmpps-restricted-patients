28b22edb4bd458c9efceefb178c242cb7a8f93ab
Peter Phillips <peter@greenthistle.co.uk>
2023-09-27 11:22:53 +0100

    SDIT-1108: ✨ Add in frontend components

--- integration_tests/pages/homepage.js
+++ integration_tests/pages/homepage.js
@@ -2,16 +2,16 @@ const page = require('./page')
 
 const homePage = () =>
   page('Manage restricted patients', {
-    loggedInName: () => cy.get('[data-test="logged-in-name"]'),
-    activeLocation: () => cy.get('[data-test="active-location"]'),
+    loggedInName: () => cy.get('[data-qa="header-user-name"]'),
+    activeLocation: () => cy.get('[data-qa="header-active-case-load"]'),
     manageAccountLink: () => cy.get('[data-test="manage-account-link"]'),
-    changeLocationLink: () => cy.get('[data-test="change-location-link"]'),
+    changeLocationLink: () => cy.get('[data-qa="changeCaseLoad"]'),
     searchRestrictedPatient: () => cy.get('[data-test="search-restricted-patient"]'),
     moveToHospital: () => cy.get('[data-test="move-to-hospital"]'),
     addRestrictedPatient: () => cy.get('[data-test="add-restricted-patient"]'),
     removeFromRestrictedPatients: () => cy.get('[data-test="remove-from-restricted-patients"]'),
     helpLink: () => cy.get('[data-test="help"]'),
-    signOutLink: () => cy.get('[data-test="sign-out-link"]'),
+    signOutLink: () => cy.get('[data-qa="signOut"]'),
     feedbackBanner: () => cy.get('[data-test="feedback-banner"]'),
   })
 
--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,35 +1,45 @@
-import express, { Router, Response } from 'express'
+import express, { Router, Request, Response, NextFunction } from 'express'
 import helmet from 'helmet'
 import crypto from 'crypto'
+import config from '../config'
 
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
 
-  router.use((req, res, next) => {
+  router.use((_req: Request, res: Response, next: NextFunction) => {
     res.locals.cspNonce = crypto.randomBytes(16).toString('base64')
     next()
   })
 
-  // Secure code best practice - see:
-  // 1. https://expressjs.com/en/advanced/best-practice-security.html,
-  // 2. https://www.npmjs.com/package/helmet
+  // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
+  const scriptSrc = [
+    "'self'",
+    (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+    'code.jquery.com',
+    '*.googletagmanager.com',
+    'www.google-analytics.com',
+    "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+  ]
+  const styleSrc = ["'self'", 'code.jquery.com', "'unsafe-inline'"]
+  const imgSrc = ["'self'", '*.googletagmanager.com', '*.google-analytics.com', 'code.jquery.com']
+  const fontSrc = ["'self'"]
+
+  if (config.apis.frontendComponents.url) {
+    scriptSrc.push(config.apis.frontendComponents.url)
+    styleSrc.push(config.apis.frontendComponents.url)
+    imgSrc.push(config.apis.frontendComponents.url)
+    fontSrc.push(config.apis.frontendComponents.url)
+  }
+
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
-          // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: [
-            "'self'",
-            (req, res: Response) => `'nonce-${res.locals.cspNonce}'`,
-            'code.jquery.com',
-            '*.googletagmanager.com',
-            'www.google-analytics.com',
-            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
-          ],
-          styleSrc: ["'self'", 'code.jquery.com', "'unsafe-inline'"],
-          fontSrc: ["'self'"],
-          imgSrc: ["'self'", '*.googletagmanager.com', '*.google-analytics.com', 'code.jquery.com'],
+          scriptSrc,
+          styleSrc,
+          fontSrc,
+          imgSrc,
           connectSrc: ["'self'", '*.googletagmanager.com', '*.google-analytics.com', '*.analytics.google.com'],
         },
       },
