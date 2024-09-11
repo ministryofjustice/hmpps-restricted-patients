ebc1044b9f40bc50f304590721c7af39a5367a12
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-09-11 16:17:52 +0100

    SDIT-2037: 🐛 Rewrite javascript after esbuild introduction

--- assets/js/open-help-section.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-  // Scroll to the first open detail
-  document.querySelector('details[open]').scrollIntoView()
-}
--- esbuild/esbuild.config.js
+++ esbuild/esbuild.config.js
@@ -36,10 +36,6 @@ const buildConfig = {
         from: path.join(cwd, 'assets/images/**/*'),
         to: path.join(cwd, 'dist/assets/images'),
       },
-      {
-        from: path.join(cwd, 'assets/js/**/*'),
-        to: path.join(cwd, 'dist/assets/js'),
-      },
     ],
     clear: glob.sync([path.join(cwd, 'dist/assets/{css,js}')]),
   },
